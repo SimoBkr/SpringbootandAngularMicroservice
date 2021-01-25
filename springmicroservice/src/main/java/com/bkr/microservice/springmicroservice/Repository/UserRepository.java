b36b11d2b4bcff93cca817d5d8c29f713edd58aa
Mohamed BAKADIR <simobkr.sb@gmail.com>
2021-01-25 21:07:28 +0100

    add JPQL

--- springmicroservice/src/main/java/com/bkr/microservice/springmicroservice/Repository/UserRepository.java
+++ springmicroservice/src/main/java/com/bkr/microservice/springmicroservice/Repository/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<UserEntity,Long> {
 
@@ -15,14 +16,24 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity,Lo
 
     UserEntity findByUserId(String userId);
 
-    @Query(value ="SELECT * FROM users",nativeQuery=true)
+//    @Query(value ="SELECT * FROM users",nativeQuery=true)
+//    Page<UserEntity> findAllUsers(Pageable pageableRequest);
+
+    @Query(value ="SELECT * from users " , nativeQuery=true)
     Page<UserEntity> findAllUsers(Pageable pageableRequest);
+
+    //Syntaxe for JPQL
+//    @Query(value ="SELECT user from UserEntity user")
+//    Page<UserEntity> findAllUsers(Pageable pageableRequest);
 
 
 //    @Query(value ="SELECT * FROM users u WHERE (u.user_name = ?1 OR u.last_name = ?1) AND u.email_verification_status = ?2",nativeQuery=true)
 //    Page<UserEntity> findAllByCriteria(Pageable page , String search,int status);
 
-    @Query(value ="SELECT * FROM users u WHERE (u.user_name = :search OR u.last_name = :search) AND u.email_verification_status = :status",nativeQuery=true)
+//    @Query(value ="SELECT * FROM users u WHERE (u.user_name = :search OR u.last_name = :search) AND u.email_verification_status = :status",nativeQuery=true)
+//    Page<UserEntity> findAllByCriteria(Pageable page , @Param("search") String search,@Param("status") int status);
+
+    @Query(value ="SELECT * FROM users u WHERE (u.user_name LIKE %:search% OR u.last_name LIKE %:search%) AND u.email_verification_status = :status",nativeQuery=true)
     Page<UserEntity> findAllByCriteria(Pageable page , @Param("search") String search,@Param("status") int status);
 
 }
